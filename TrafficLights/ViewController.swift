5fb50668b43479eba0af3c964485469a1e5e2022
Alexander Barabash <alexanderbarabash86@gmail.com>
2022-03-03 13:31:30 +0300

    Method if

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -27,7 +27,18 @@ class ViewController: UIViewController {
     }
 
     @IBAction func startButtonPress() {
-    }
+
+        if redSignal.alpha == 0.3 {
+            redSignal.alpha = 1
+        } else if redSignal.alpha == 1 {
+            redSignal.alpha = 0.3
+            yellowSignal.alpha = 1
+        } else if yellowSignal.alpha == 1 {
+            yellowSignal.alpha = 0.3
+            greenSignal.alpha = 1
+        } else if greenSignal.alpha == 1 {
+            greenSignal.alpha = 0.3
+        }
     
 }
-
+}
